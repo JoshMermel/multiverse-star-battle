feddd9ca84f436c30f4a2326cbac3bd07e57b4dc
Josh Mermelstein <joshmermelstein@Joshs-MacBook-Pro.local>
2026-02-22 21:05:11 -0500

    more defense against early toast dismissal

--- script.js
+++ script.js
@@ -400,8 +400,13 @@ STAR BATTLE RULES:
     toast.textContent = message;
     toast.className = '';
     toast.classList.add(`toast-${type}`);
+    this.toastBirthTime = Date.now();
+
     toast.classList.remove('toast-hidden');
     toast.onclick = () => {
+      if (Date.now() - this.toastBirthTime < 500) {
+        return;
+      }
       toast.classList.add('toast-hidden');
     };
     clearTimeout(this.toastTimeout);
