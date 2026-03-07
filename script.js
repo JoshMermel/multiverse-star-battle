7bdbc30ced53343a65234cf4deca7fdd7defc6ae
Josh Mermelstein <joshmermelstein@Joshs-MacBook-Pro.local>
2026-03-07 13:11:46 -0500

    update puzzles :)

--- script.js
+++ script.js
@@ -146,7 +146,7 @@ STAR BATTLE RULES:
 
   loadPuzzle(puzzleData, categoryId) {
     // Save the reference to the current puzzle data
-    this.currentPuzzleUniqueId = `${categoryId}_${puzzleData.id}_v2`;
+    this.currentPuzzleUniqueId = `${categoryId}_${puzzleData.id}_v3`;
     this.currentPuzzle = puzzleData;
 
     // Map data to game properties
